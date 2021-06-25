b3f9b5f4824ae8dd56ed3b123e34ef310ae599fe
ZzicC12 <pyw251@gmail.com>
2021-06-25 13:33:07 +0900

    add morgan

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(morgan("tiny"));
+app.get("/", (req, res) => res.send("home"));
 
 const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT}`);
